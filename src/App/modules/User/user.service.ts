1c99012dcebcd1cec7ec838f208bf5fa95a8fc58
safi islam <118913244+safiislam@users.noreply.github.com>
2024-09-24 16:32:16 +0600

    add

--- src/App/modules/User/user.service.ts
+++ src/App/modules/User/user.service.ts
@@ -1,3 +1,4 @@
+import httpStatus from "http-status";
 import config from "../../config";
 import AppError from "../../errors/appError";
 import { TUser } from "./user.interface";
@@ -18,15 +19,19 @@ const createUserIntoDB = async (userData: TUser) => {
 }
 const getMeFromDB = async (userData: Partial<TUser>) => {
     const user = await User.findOne({ userName: userData.userName })
+
+    if (!user) {
+        throw new AppError(404, "User doesn't exist")
+    }
     const isPasswordTrue = await bcrypt.compare(userData.password as string, user?.password as string)
-    console.log(isPasswordTrue);
     if (isPasswordTrue) {
         const jwtData = { userName: user?.userName, role: userData.role }
         const result = jwt.sign(jwtData, config.jwt_secret as string,)
         console.log(result);
         return { token: result }
-
-
+    }
+    else {
+        throw new AppError(httpStatus.FORBIDDEN, 'Wrong password')
     }
 }
 
